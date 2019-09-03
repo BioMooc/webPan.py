a118fc6647319ad97b9c0c67873cb88ebd544d76
jimmy <JimmyMall@live.com>
2019-09-03 17:10:13 +0800

    v 0.0.1 只能列举运行目录的地址。

---
+++ webPan.py
@@ -0,0 +1,93 @@
+import flask, os,sys,time,re
+from webPanLib import *
+from flask import request, send_from_directory,redirect
+
+interface_path = os.path.dirname(__file__)
+sys.path.insert(0, interface_path)  #将当前文件的父目录加入临时系统变量
+
+
+server = flask.Flask(__name__)
+
+
+#get方法：指定目录下载文件
+@server.route('/download', methods=['get'])
+def download():
+    fpath = request.values.get('path', '') #获取文件路径
+    fname = request.values.get('filename', '')  #获取文件名
+    if fname.strip() and fpath.strip():
+        print(fname, fpath);
+        if os.path.isfile(os.path.join(fpath,fname)) and os.path.isdir(fpath):
+            return send_from_directory(fpath, fname, as_attachment=True) #返回要下载的文件内容给客户端
+        else:
+            return '{"msg2":"参数不正确"}path=%s, filename=%s;' %(fpath, fname);
+    else:
+        return '{"msg1":"请输入参数"}'
+#
+
+@server.route('/', methods=['get'])
+def index():
+    return redirect("/list");
+#
+
+
+
+# get方法：查询当前路径下的所有文件
+@server.route('/list', methods=['get'])
+def getfiles():
+    fpath = request.values.get('fpath', '.') #获取用户输入的目录
+    #print(fpath)
+    #str to arr, by the end of path
+    sep=fpath[-1];
+    arr=re.split(sep, fpath);
+    #arr to a links
+    url="/list?fpath="
+    urlPath="<a class=root href=/list>根目录</a> "
+    for i in range(len(arr)-1 ):
+        url=url+arr[i]+"/"
+        urlPath+="<a href="+url+">"+arr[i]+"</a>/"
+        #print(i, arr[i], urlPath)
+    titlePath="<h4>Path: "+urlPath+"</h4>\n\n"; #cut to pieces.
+
+    #
+    htmlF="";
+    htmlD="";
+    if os.path.isdir(fpath):
+        filelist = os.listdir(fpath)
+        for file in filelist:
+            url=os.path.join(fpath, file);
+            fTime=getModifiedTime(url);
+            #
+            if os.path.isfile(url): #type="file"
+                fSize=getDocSize(url);
+                htmlF+="<li class=file>"+imgFile+file+"<a target='_blank' href='/download?filename="+file+"&path="+fpath+"'>下载</a> <span>"+fSize+"</span>   <span>"+fTime+"</span>  </li>"
+            if os.path.isdir(url): #type="dir"
+                htmlD+="<li class=dir>"+imgDir+file+"/ <a href='/list?fpath="+url+"/'>打开</a>   <span>"+fTime+"</span>  </li>"
+        #files = [file for file in filelist if os.path.isfile(os.path.join(fpath, file))]
+    return title+css+titlePath+"<ol>"+htmlF+htmlD+"</ol>";
+
+
+# post方法：上传文件的
+@server.route('/upload', methods=['post'])
+def upload():
+    fname = request.files.get('file')  #获取上传的文件
+    if fname:
+        t = time.strftime('%Y%m%d%H%M%S')
+        new_fname = r'upload/' + t + fname.filename
+        fname.save(new_fname)  #保存文件到指定路径
+        return '{"code": "ok"}'
+    else:
+        return '{"msg": "请上传文件！"}'
+
+
+
+# 添加新静态文件的路径，这样就允许data/下的图片加载了
+@server.route("/static/<path:filename>")
+def downloader(filename):
+    return send_from_directory("static",filename,as_attachment=False)
+
+
+
+# run the app
+if __name__ == '__main__':
+    server.debug = True # 设置调试模式，生产模式的时候要关掉debug
+    server.run(host="127.0.0.1",port=8005) #default
---
+++ webPanLib.py
@@ -0,0 +1,49 @@
+import os,time
+
+
+title="<title>webPan.py - Mini网盘</title>";
+css='<link rel="stylesheet" href="/static/css/webPan.css" />\n\n'
+
+imgFile="""
+<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACAklEQVR42u2XyUsDMRTGe3S5OpNSipP0oEkKXgQRBBE33BARRYqIIIKIIIgIggcR3HBB646IuG+H4oK1tC4V/Z96rO+BcxEmLm3m5IM5DUN+yfu+72U8ni9VEOBFxApWk0JZYwZErclEncl4vUGDDYYlmgos0UwCvIUw2WpS0WZavN1gwQ6DyU5CRRehPORlsoxSmuP5S+Hi6QzqKnKTTr69RwCm/E8QuPNMAVKpVPr59f3aLCyu8Pv9ub8CwGPPFAALIRJPyTuDicpfQWC/swFgQ8Sfkvdeyqt8vtK8HwGg2LIFYEPEEs8PeLI/gjAs2ZhNABviPhaP4+l+C4FWyzaADXEXjT3iBgkpyXd2AXhcB4ANcRuNveAazgAQMJkCzC2Fv32cXQDpltZcBuMzzgAQrboBYJPzziKEXNcOYPFFZw3AQNEOwOSyAoCHtGuAilWFBmS3bgBi8TXFLBA9+lsgNhSzgPe64IIt1Szo0w/AdxRJKPr121DsKpJQDuhPQrGniuJB7S6gfF81jodcEOGBwgVy2AUNHKmieMQFgGPVNBzV3wJ5qkrCMRc0cKZKwnEXhtGF4tdMTOi3obhU3oohKiehT1MQGNOEylm8wYBwFnCOA/0K+DhMLLkO7dqEd9sYrZhuGDDocbQZPIeodriKn+CRw3fnuHNcHNPW81+f9QFiij/qVroAPQAAAABJRU5ErkJggg==" border="0" width="16" height="16">
+"""
+imgDir="""
+<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABn0lEQVR42u2XvUsCYRzHH5WgN3r5K5qDotGhpamtrSFobGxrEWqwUI9DQwOJIFoSWsrTpEFEMgQjwigSkSQxTau78336dScYij5Kl7+jwS98hufgnu+H51l+DyGD9CEGA9G6jWS6Gy4X0aGUcwyZ4xgt+PanuuJhdBJE39dyr5nMy+XxWwf0ylPEWZfgTGShz+UH9YJsPgfpbKYrDzdWkP/xWMhs24YARNPrHhsoKW8QvTYDZ9GAlyUzLeXyx1732IyS8gb3gR3pJAj7I3C1SyZ9tgn4bZSUy7ymYtIpEL5F4FIS4AVB0YZ/EnBbiF1agBqEL9baBXy2cVArskRHgVohBKX8MTp0ATEIpfdDdCgCY1Dj/VB6c6BDFah+XkExxaJDF/jwQDG5hw5dIHcOhcQ2Op0FrKNQzZxBIbaFDlWgkj4F8XETHbpA6gTE6AY6FIERqCSPQLxbR4cqUH5xghBZRYcukLCDEF5Bhy4QtwIfWkaHLhBjgA8uoUMRGIbyswn4wCI6nQXYIfjy61WhaTjhpZnQqPpEJHcN3pH/Lt8WJRscHbhvPQAAAABJRU5ErkJggg==" border="0" width="16" height="16">
+"""
+
+
+# 字节bytes转化kb\m\g
+#v0.2 保留2位
+def formatSize(bytes):
+    try:
+        bytes = float(bytes)
+        kb = bytes / 1024
+    except:
+        print("传入的字节格式不对")
+        return "Error"
+
+    if kb >= 1024:
+        M = kb / 1024
+        if M >= 1024:
+            G = M / 1024
+            return "%.2fG" % ( round(G,2) )
+        else:
+            return "%.2fM" % ( round(M,2) )
+    else:
+        return "%.2fkb" % ( round(kb,2) )
+
+
+# 获取文件大小
+def getDocSize(path):
+    try:
+        size = os.path.getsize(path)
+        return formatSize(size)
+    except Exception as err:
+        print(err)
+#
+
+#获取修改时间
+def getModifiedTime(full_path):
+    mtime = os.stat(full_path).st_mtime;
+    file_modify_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(mtime));
+    return file_modify_time
